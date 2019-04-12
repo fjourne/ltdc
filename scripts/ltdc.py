f38c1a7d6fd11421f24cc46f2948f0e753f14f0a
Efelrine <efelrine@gmail.com>
2019-04-12 17:09:00 +0200

    Lecture des pages individuelles des persos by Piell

--- scripts/ltdc.py
+++ scripts/ltdc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # coding: utf-8
 import sys
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -9,34 +10,35 @@ import re
 from graphviz import Graph
 
 cookie = {
-    'Cookie': 'wiki_wikiUserID=7; wiki_wikiUserName=Fabien;'
-              + ' wiki_wikiToken=8e8703c517db1ef0343181e2da1a2224; '
-              + 'ltdc_pppe_wikiUserID=7;'
-              + ' ltdc_pppe_wikiUserName=Fabien;'
-              + ' ltdc_pppe_wikiToken=8e8703c517db1ef0343181e2da1a2224;'
-              + ' wiki_wiki_session=c52v051pcbu9e26clhrnjivr8941sd20;'
-              + ' ltdc_pppe_wiki_session=tgk3v637iovmj0vve6cp8rv4h1snp03d'}
-page_perso = requests.get("http://louiki.elyseum.fr/ltdc_pppe/index.php?title=Personnages&action=edit",
+    'Cookie': 'wiki_wikiUserID=4; wiki_wikiUserName=Piell;'
+              + ' wiki_wikiToken=c36a79b16a31568dbedd74442315fd0d; '
+              + 'ltdc_pppe_wikiUserID=4;'
+              + ' ltdc_pppe_wikiUserName=Piell;'
+              + ' ltdc_pppe_wikiToken=c36a79b16a31568dbedd74442315fd0d;'
+              + ' wiki_wiki_session=ilmc6k7e2vefmor20lmlg9k7lpjbpg6;'
+              + ' ltdc_pppe_wiki_session=h0jvm1nh9cq1dp8gjhdl8uh2c23mvknp;'}
+
+page_tableau_perso = requests.get("https://louiki.elyseum.fr/ltdc_pppe/index.php/Tableau_des_Personnages",
                           headers=cookie).text
 
-page_groupe = requests.get("http://louiki.elyseum.fr/ltdc_pppe/index.php?title=Groupes_et_organisations&action=edit",
+page_groupe = requests.get("https://louiki.elyseum.fr/ltdc_pppe/index.php?title=Groupes_et_organisations&action=edit",
                            headers=cookie).text
 
-page_trame = requests.get("http://louiki.elyseum.fr/ltdc_pppe/index.php?title=Trames&action=edit", headers=cookie).text
+page_trame = requests.get("https://louiki.elyseum.fr/ltdc_pppe/index.php?title=Trames&action=edit", headers=cookie).text
 
-page_event = requests.get("http://louiki.elyseum.fr/ltdc_pppe/index.php?title=Trames_locales&action=edit",
+page_event = requests.get("https://louiki.elyseum.fr/ltdc_pppe/index.php?title=Trames_locales&action=edit",
                           headers=cookie).text
 
-page_lieu = requests.get("http://louiki.elyseum.fr/ltdc_pppe/index.php?title=G%C3%A9ographie&action=edit",
+page_lieu = requests.get("https://louiki.elyseum.fr/ltdc_pppe/index.php?title=G%C3%A9ographie&action=edit",
                          headers=cookie).text
 
-page_role = requests.get("http://louiki.elyseum.fr/ltdc_pppe/index.php?title=Connaissances_diverses&action=edit",
+page_role = requests.get("https://louiki.elyseum.fr/ltdc_pppe/index.php?title=Connaissances_diverses&action=edit",
                          headers=cookie).text
 
-page_materiel = requests.get("http://louiki.elyseum.fr/ltdc_pppe/index.php?title=Mat%C3%A9riel&action=edit",
+page_materiel = requests.get("https://louiki.elyseum.fr/ltdc_pppe/index.php?title=Mat%C3%A9riel&action=edit",
                              headers=cookie).text
 
-page_edge = requests.get("http://louiki.elyseum.fr/ltdc_pppe/index.php?title=Liens&action=edit", headers=cookie).text
+page_edge = requests.get("https://louiki.elyseum.fr/ltdc_pppe/index.php?title=Liens&action=edit", headers=cookie).text
 
 
 dot = Graph(name='Dragons', filename='./tmp/derniersDragons.gv', comment='Les Derniers Dragons', strict=True)
@@ -78,7 +80,7 @@ def line_to_list(line, search):
     """
     :param line: le texte de la ligne
     :param search: le type de lien rechercher dans la ligne (g, gr, t, tl, ...)
-    :return: la liste des liens trouvers dans la ligne
+    :return: la liste des liens trouvés dans la ligne
     """
     return re.findall("[^\{]*(\{\{" + search + "\|[^\}]+\}\})", line)
 
@@ -119,17 +121,42 @@ def add_nodes_types_shape(types, shape, linkname, peripheries='1', linkpers='', 
         if linkname != '':
             dot.node(nomgraph, nom, shape=shape, link=linkname + nom, peripheries=peripheries, fillcolor=color)
         elif linkpers != '':
-            lien = re.findall("lien=\{\{.*\|(.*)\}\}\n", t)[0]
-            dot.node(nomgraph, nom, shape=shape, link=linkpers + lien, peripheries=peripheries, fillcolor=color)
+            liens = re.findall("lien=\{\{.*\|(.*)\}\}\n", t)
+            if not liens:
+                lien = "https://louiki.elyseum.fr/ltdc_pppe/index.php/" + re.findall("lien=\[\[(.*)\]\]\n", t)[0]
+            else:
+                lien = linkpers + liens[0]
+            dot.node(nomgraph, nom, shape=shape, link=lien, peripheries=peripheries, fillcolor=color)
         else:
             dot.node(nomgraph, nom, shape=shape, link=link, peripheries=peripheries, fillcolor=color)
     return
 
 
-personnages = page_perso.split("{{Personnage")
-personnages.pop(0)
-p_link_to_nomgraphe = map_link_nomgraph(personnages)
+tableau_personnages = page_tableau_perso.split("<table class=\"wikitable sortable\">")
+tableau_personnages.pop(0)
+tableau_personnages = tableau_personnages[0].split("</table>")
+tableau_personnages = tableau_personnages[0].split("<tr>")
+tableau_personnages.pop(0)
 
+page_p = requests.get("https://louiki.elyseum.fr/ltdc_pppe/index.php?title=Personnages&action=edit",
+                      headers=cookie).text
+personnages = page_p.split("{{Personnage")
+personnages.pop(0)
+
+for p in tableau_personnages:
+    links = re.findall("<a.*>(.*)</a>", p)
+    if links:
+        # Le premier lien est celui du nom du personnage
+        perso = links[0]
+        link_perso = re.findall("<a href=\"/ltdc_pppe/index.php/(.*)\" .*>.*</a>", p)[0]
+        if "Personnages" not in link_perso:
+            page_p = requests.get("https://louiki.elyseum.fr/ltdc_pppe/index.php?title="+link_perso+"&action=edit",
+                                  headers=cookie).text
+            template1 = page_p.split("{{Personnage")
+            template1.pop(0)
+            personnages = personnages + template1
+
+p_link_to_nomgraphe = map_link_nomgraph(personnages)
 
 groupes = page_groupe.split("{{Groupe")
 groupes.pop(0)
@@ -169,18 +196,18 @@ categories = [
 ]
 
 # Ajouts des nodes persos
-add_nodes_types_shape(personnages, 'ellipse', '', '1', 'http://louiki.elyseum.fr/ltdc_pppe/index.php/Personnages#')
+add_nodes_types_shape(personnages, 'ellipse', '', '1', 'https://louiki.elyseum.fr/ltdc_pppe/index.php/Personnages#')
 
 # Ajouts des nodes groupes
-add_nodes_types_shape(groupes, 'ellipse', 'http://louiki.elyseum.fr/ltdc_pppe/index.php/Groupes_et_organisations#', '2')
+add_nodes_types_shape(groupes, 'ellipse', 'https://louiki.elyseum.fr/ltdc_pppe/index.php/Groupes_et_organisations#', '2')
 
 # Ajouts des nodes trames, events, lieux, objets, roles
-add_nodes_types_shape(trames, 'octagon', 'http://louiki.elyseum.fr/ltdc_pppe/index.php/Trames#', '2')
-add_nodes_types_shape(events, 'octagon', 'http://louiki.elyseum.fr/ltdc_pppe/index.php/Trames_locales#')
-add_nodes_types_shape(lieux, 'box', 'http://louiki.elyseum.fr/ltdc_pppe/index.php/G%C3%A9ographie#')
-add_nodes_types_shape(materiels, 'invhouse', 'http://louiki.elyseum.fr/ltdc_pppe/index.php/Mat%C3%A9riel#')
+add_nodes_types_shape(trames, 'octagon', 'https://louiki.elyseum.fr/ltdc_pppe/index.php/Trames#', '2')
+add_nodes_types_shape(events, 'octagon', 'https://louiki.elyseum.fr/ltdc_pppe/index.php/Trames_locales#')
+add_nodes_types_shape(lieux, 'box', 'https://louiki.elyseum.fr/ltdc_pppe/index.php/G%C3%A9ographie#')
+add_nodes_types_shape(materiels, 'invhouse', 'https://louiki.elyseum.fr/ltdc_pppe/index.php/Mat%C3%A9riel#')
 add_nodes_types_shape(roles, 'pentagon', '', '1', '',
-                      'http://louiki.elyseum.fr/ltdc_pppe/index.php/Connaissances_diverses#Tarot_de_Targ.C3.A8ne')
+                      'https://louiki.elyseum.fr/ltdc_pppe/index.php/Connaissances_diverses#Tarot_de_Targ.C3.A8ne')
 
 error = ""
 
